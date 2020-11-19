6717c2244fc45df58ed36e3e36987bf10726d443
barry <barryme2049@gmail.com>
2020-11-19 17:57:19 +0800

    test ipv6

--- b52-sdk/app/src/main/java/com/mobile/app/sdk/MyApp.kt
+++ b52-sdk/app/src/main/java/com/mobile/app/sdk/MyApp.kt
@@ -1,6 +1,7 @@
 package com.mobile.app.sdk
 
 import androidx.multidex.MultiDexApplication
+import com.mobile.sdk.ipv6.Ipv6X
 import com.mobile.sdk.sister.SisterX
 
 class MyApp : MultiDexApplication() {
@@ -8,6 +9,7 @@ class MyApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         SisterX.setup(this, BuildConfig.DEBUG)
+        Ipv6X.setup(this, BuildConfig.DEBUG)
         SisterX.setServers("java.cg.xxx", "java.cg.xxx")
     }
 }
