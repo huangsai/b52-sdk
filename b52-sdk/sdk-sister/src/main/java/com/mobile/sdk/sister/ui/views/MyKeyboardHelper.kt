4e9153b24c495c4e769bf9feca08f333b424127d
barry <barryme2049@gmail.com>
2020-11-20 19:59:18 +0800

    优化悬浮窗

--- b52-sdk/sdk-sister/src/main/java/com/mobile/sdk/sister/ui/views/MyKeyboardHelper.kt
+++ b52-sdk/sdk-sister/src/main/java/com/mobile/sdk/sister/ui/views/MyKeyboardHelper.kt
@@ -2,10 +2,13 @@ package com.mobile.sdk.sister.ui.views
 
 import android.app.Dialog
 import android.graphics.Rect
+import android.view.MotionEvent
+import android.view.View
+import com.lzf.easyfloat.interfaces.OnFloatCallbacks
 import com.mobile.guava.android.context.dp2px
 import kotlin.math.roundToInt
 
-object MyKeyboardHelper {
+object MyKeyboardHelper:OnFloatCallbacks {
     
     fun isKeyboardVisible(dialog: Dialog): Boolean {
         val r = Rect()
@@ -14,5 +17,26 @@ object MyKeyboardHelper {
         activityRoot.getWindowVisibleDisplayFrame(r)
         val heightDiff = activityRoot.rootView.height - r.height()
         return heightDiff > visibleThreshold
+    }
+
+    override fun createdResult(isCreated: Boolean, msg: String?, view: View?) {
+    }
+
+    override fun dismiss() {
+    }
+
+    override fun drag(view: View, event: MotionEvent) {
+    }
+
+    override fun dragEnd(view: View) {
+    }
+
+    override fun hide(view: View) {
+    }
+
+    override fun show(view: View) {
+    }
+
+    override fun touchEvent(view: View, event: MotionEvent) {
     }
 }
