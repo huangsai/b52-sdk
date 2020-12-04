c1a022b213f8d8ac59ed2b0ac5b550d60aeadc5b
barry <barryme2049@gmail.com>
2020-12-04 17:12:09 +0800

    call模块导致编译失败

--- b52-sdk/app/src/main/java/com/mobile/app/sdk/MyApp.kt
+++ b52-sdk/app/src/main/java/com/mobile/app/sdk/MyApp.kt
@@ -1,7 +1,6 @@
 package com.mobile.app.sdk
 
 import androidx.multidex.MultiDexApplication
-import com.mobile.sdk.call.CallX
 import com.mobile.sdk.sister.SisterX
 
 class MyApp : MultiDexApplication() {
@@ -9,8 +8,7 @@ class MyApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         SisterX.setup(this, BuildConfig.DEBUG)
-        SisterX.setServers("java.cg.xxx:31301")
-
-//        CallX.setup(this, BuildConfig.DEBUG)
+        SisterX.setServers("java.cg.xxx:30311")
+        // SisterX.setServers("java.cg.xxx:31301")
     }
 }
